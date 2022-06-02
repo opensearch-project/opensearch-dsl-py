9d961830701f9dca5e004c5957a6b4559945bf13
Vacha Shah <vachshah@amazon.com>
2022-06-02 15:55:37 +0000

    Bump client version to 2.0.0
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from os.path import dirname, join
 
 from setuptools import find_packages, setup
 
-VERSION = (1, 0, 0)
+VERSION = (2, 0, 0)
 __version__ = VERSION
 __versionstr__ = ".".join(map(str, VERSION))
 
