3e16b7829b98681bd22b7adfb79abe196ab4d01a
Vacha Shah <vachshah@amazon.com>
2022-07-12 12:40:27 -0700

    Fixing development status for releases (#63)
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     packages=find_packages(where=".", exclude=("tests*",)),
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
